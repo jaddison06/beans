3117330506ec76c69ff271ec50e7ef0c0508e33d
James Addison <addison.j@etoncollege.org.uk>
2022-03-24 15:36:52 +0000

    okay yeah whatever that means

--- build/commandline.py
+++ build/commandline.py
@@ -25,17 +25,55 @@ class Function:
 @dataclass
 class Noun:
     name: str
-    key: str | None # defaults to None
+    key: str | None           # defaults to None
 
-    attrs: list[Attr] # defaults to empty
+    dynamicAttrs: list[str]   # defaults to empty
+    attrs: list[Attr]         # defaults to empty
+    functions: list[Function] # defaults to empty
 
-def parseGrammar(grammarFile: str) -> list[Noun]: ...
+def boolParam(param: str) -> bool: return param.lower() in ['y', 'yes', 't', 'true']
+
+def parseGrammar(grammarFile: str) -> list[Noun]:
+    out: list[Noun] = []
+
+    root = ET.parse(grammarFile).getroot()
+
+    for noun in root:
+        if noun.tag != 'noun': continue
+        nounName = noun.attrib['name']
+        nounKey = noun.attrib.get('key', None)
+        dynamicAttrs: list[str] = []
+        attrs: list[Attr] = []
+        functions: list[Function] = []
+        for member in noun:
+            if member.tag == 'dynamicattrs':
+                dynamicAttrs.append(member.attrib['name'])
+            elif member.tag == 'attr':
+                attrName = member.attrib['name']
+                type_ = member.attrib['type']
+                get = boolParam(member.attrib.get('get', 'true'))
+                set_ = boolParam(member.attrib.get('set', 'true'))
+                attrKey = member.attrib.get('key', None)
+                attrs.append(Attr(attrName, type_, get, set_, attrKey))
+            elif member.tag == 'function':
+                memberName = member.attrib['name']
+                params: list[Param] = []
+                for param in member:
+                    paramName = param.attrib['name']
+                    type_ = param.attrib['type']
+                    optional = boolParam(param.attrib.get('optional', 'false'))
+                    params.append(Param(paramName, type_, optional))
+                functions.append(Function(memberName, params))
+        out.append(Noun(nounName, nounKey, dynamicAttrs, attrs, functions))
+    
+    return out
+
 
 
 def codegen(grammar: list[Noun]) -> str:
     buf = StringIO()
 
-
+    print(grammar)
 
     return buf.getvalue()
 
